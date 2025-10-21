219d5b8479e7d1d8becb6c482b16d09efaaf6514
deewyz <134692502+deewyz@users.noreply.github.com>
2025-10-21 17:44:17 +0200

    paolo pagina pazienti 2

--- src/pages/logged/Pazienti.tsx
+++ src/pages/logged/Pazienti.tsx
@@ -5,7 +5,7 @@ function PazientiPage() {
     <div>
       <div className="mb-8 flex items-center justify-between-center gap-218">
         <h1 className="text-2xl font-semibold">Lista pazienti</h1>
-        <button className="rounded bg-primary px-4 py-2 text-foreground hover:bg-primary/90">
+        <button className="rounded bg-primary px-4 py-2 text-primary-foreground">
           Aggiungi nuovo paziente
         </button>
       </div>
