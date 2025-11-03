26f7fb925190d1516f5e3b37afb94aad2aa8b703
luigipierro <146242520+luigipierro@users.noreply.github.com>
2025-11-03 09:57:14 +0100

    fix nome

--- src/components/custom/nav-user.tsx
+++ src/components/custom/nav-user.tsx
@@ -41,6 +41,13 @@ export function NavUser({
   const { isMobile } = useSidebar()
   const navigate = useNavigate();
   const { logout } = useAuth();
+
+  const getInitials = (name: string) => {
+    const names = name.split(' ');
+    if (names.length > 1) return `${names[0][0]}${names[1][0]}`.toUpperCase();
+    return name.substring(0, 2).toUpperCase();
+  }
+
   return (
     <SidebarMenu>
       <SidebarMenuItem>
@@ -52,7 +59,7 @@ export function NavUser({
             >
               <Avatar className="h-8 w-8 rounded-lg">
                 <AvatarImage src={user.avatar} alt={user.name} />
-                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                <AvatarFallback className="rounded-lg">{getInitials(user.name)}</AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-medium">{user.name}</span>
@@ -71,7 +78,9 @@ export function NavUser({
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
                   <AvatarImage src={user.avatar} alt={user.name} />
-                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                  <AvatarFallback className="rounded-lg">
+                    {getInitials(user.name)}
+                  </AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-medium">{user.name}</span>
